2546f5defee2ae10890e23780c795d7284f5b315
dawid.pocztowski <dawid.pocztowski@PK2-09>
2019-06-01 15:56:41 +0200

    Added game over check

--- KolokiKrzyzyk/Program.cs
+++ KolokiKrzyzyk/Program.cs
@@ -23,11 +23,12 @@ namespace KolokiKrzyzyk
                 { '4', '5', '6' },
                 { '7', '8', '9' }
             };
+            char[,] planszaKopia = plansza.Clone() as char[,];
 
             //Petla na kolejne ruchy graczy
             bool koniecGry = false;
             bool ruchGraczaA = true;
-            while (!koniecGry)
+            for (int runda = 0; runda < plansza.Length; ++runda)
             {
                 Console.Clear();
                 RysujPlansze(plansza);
@@ -35,13 +36,13 @@ namespace KolokiKrzyzyk
                 if (ruchGraczaA)
                 {
                     Console.WriteLine("Ruch wykonuje: " + gA.Imie);
-                    koniecGry = gA.WykonajRuch(plansza);
+                    koniecGry = gA.WykonajRuch(plansza, planszaKopia);
                     ruchGraczaA = false;
                 }
                 else
                 {
                     Console.WriteLine("Ruch wykonuje: " + gB.Imie);
-                    koniecGry = gB.WykonajRuch(plansza);
+                    koniecGry = gB.WykonajRuch(plansza, planszaKopia);
                     ruchGraczaA = true;
                 }
 
@@ -71,28 +72,78 @@ namespace KolokiKrzyzyk
 
     interface IRuch
     {
-        bool WykonajRuch(char[,] plansza);
+        bool WykonajRuch(char[,] plansza, char[,] planszaKopia);
     }
 
     abstract class Gracz
     {
         public string Imie { get; set; }
         public char Znak { get; set; }
+
+        public bool SprawdzCzyKoniecGry(char[,] plansza)
+        {
+            int wysokosc = plansza.GetLength(0);
+            int szerokosc = plansza.GetLength(1);
+            if (szerokosc != wysokosc)
+                throw new Exception("Plansza nie jest kwartatowa!");
+
+            //Sprawdz wiersze
+            for(int i = 0; i <wysokosc; ++i)
+            {
+                int sumaWiersza = 0;
+                for(int j = 0; j < szerokosc; ++j)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaWiersza;
+                }
+                if (sumaWiersza == szerokosc)
+                    return true;
+            }
+
+            //Sprawdz kolumny
+            for (int j = 0; j < szerokosc; ++j)
+            {
+                int sumaKolumny = 0;
+                for (int i = 0; i < szerokosc; ++i)
+                {
+                    if (plansza[i, j] == Znak)
+                        ++sumaKolumny;
+                }
+                if (sumaKolumny == wysokosc)
+                    return true;
+            }
+
+            //Sprawdz przekatne
+            int sumaPrzekatnejA = 0;
+            int sumaPrzekatnejB = 0;
+            for(int k = 0; k < szerokosc; ++k)
+            {
+                if (plansza[k, k] == Znak)
+                    ++sumaPrzekatnejA;
+                if (plansza[k, szerokosc - 1 - k] == Znak)
+                    ++sumaPrzekatnejB;
+            }
+            if (sumaPrzekatnejA == szerokosc || sumaPrzekatnejB == szerokosc)
+                return true;
+
+            //Zadna z opcji, czyli jeszcze nie koniec gry
+            return false;
+        }
     }
 
     class GraczLudzki : Gracz, IRuch
     {
-        public bool WykonajRuch(char[,] plansza)
+        public bool WykonajRuch(char[,] plansza, char[,] planszaKopia)
         {
-            return false; //Do poprawienia
+            return SprawdzCzyKoniecGry(plansza);
         }
     }
 
     class GraczKomputerowy : Gracz, IRuch
     {
-        public bool WykonajRuch(char[,] plansza)
+        public bool WykonajRuch(char[,] plansza, char[,] planszaKopia)
         {
-            return false; //Do poprawienia
+            return SprawdzCzyKoniecGry(plansza);
         }
     }
 }
